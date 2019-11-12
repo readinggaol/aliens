81004374d248bec6088aed97d937d78b8992147b
doug <dougparker.m@gmail.com>
2019-11-12 12:02:47 +0800

    factored controls, fullscreen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -11,7 +11,9 @@ class alienInvasion:
 		"""Initialize game and create resources"""
 		pygame.init
 		self.settings = Settings()
-		self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+		self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+		self.settings.screen_width = self.screen.get_rect().width
+		self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption("Alien Wreckfest")
 
 		self.ship = Ship(self)
@@ -29,18 +31,25 @@ class alienInvasion:
 				if event.type == pygame.QUIT:
 					sys.exit()
 				elif event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_RIGHT:
-						self.ship.moving_right = True
-					elif event.type == pygame.KEYDOWN:
-						if event.key == pygame.K_LEFT:
-							self.ship.moving_left = True
-
+					self._check_keydown_events(event)	
 				elif event.type == pygame.KEYUP:
-					if event.key == pygame.K_RIGHT:
-						self.ship.moving_right = False	
-					elif event.type == pygame.KEYUP:
-						if event.key == pygame.K_LEFT:
-							self.ship.moving_left = False
+					self._check_keyup_events(event)
+
+	def _check_keydown_events(self, event):
+		"""Responding to keypresses"""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = True
+		if event.key == pygame.K_LEFT:
+			self.ship.moving_left = True
+		if event.key == pygame.K_q:
+			sys.exit()	
+
+	def _check_keyup_events(self, event):
+		"""Responding to keypresses"""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		if event.key == pygame.K_LEFT:
+			self.ship.moving_left = False	
 
 	def _update_screen(self):
 		"""Update images on the screen and flip to the new screen"""
