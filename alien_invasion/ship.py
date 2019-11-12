5768b54bf95ecc084236e323f06b1f3182c945c8
doug <dougparker.m@gmail.com>
2019-11-12 10:45:58 +0800

    Ship moving with boundaries

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,6 +7,7 @@ class Ship:
 		"""Initialize ship and starting position"""
 		self.screen = ai_game.screen
 		self.screen_rect = ai_game.screen.get_rect()
+		self.settings = ai_game.settings
 		
 		#load image and get rect
 		self.image = pygame.image.load('images/rocket.bmp')
@@ -14,16 +15,25 @@ class Ship:
 
 		self.rect.midbottom = self.screen_rect.midbottom
 
+		#store decimal value for the ship's horizontal position
+		self.x = float(self.rect.x)
+
 		#movement flag
 		self.moving_right = False
 		self.moving_left = False
 
 	def update(self):
 		"""Update the ship's position based on the movement flag."""
-		if self.moving_right:
-			self.rect.x += 1
-		if self.moving_left:
-			self.rect.x -= 1
+		#update ship's x value, not the rext
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > 0:
+			self.x -= self.settings.ship_speed
+
+
+
+		#update rect object from self.x
+		self.rect.x = self.x
 
 	def blitme(self):
 		"""Draw the ship at its current location"""
